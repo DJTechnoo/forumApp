e50d0bd5cd5391a75ac48c35f14d8f36eee24efd
askelej <askeltechno@hotmail.com>
2018-09-26 02:26:33 +0200

    cleanup, removed posts model

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,16 +3,13 @@
 class Pages extends Controller {
 	
 	public function __construct(){
-		$this->postModel = $this->model("Post");
+		
 	}
 	
 	
-	public function index(){
-		$posts= $this->postModel->getPosts();
-		
+	public function index(){		
 		$data = [
-			"title" => "Home",
-			"posts" => $posts
+			"title" => "Home"
 		
 		];
 		$this->view("pages/index", $data);
