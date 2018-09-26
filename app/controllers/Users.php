6b8c4f3648c17fe392fb207d82858b8e3da70343
askelej <askeltechno@hotmail.com>
2018-09-26 03:33:37 +0200

    added navbar and users class

---
+++ app/controllers/Users.php
@@ -0,0 +1,23 @@
+<?php
+
+class Users {
+	
+	public function __construct(){
+		
+	}
+
+
+	public function register(){
+		if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		
+		}else{
+			echo "Load a form<br>";
+		}
+	}
+
+
+
+}
+
+
+?>
---
+++ app/views/inc/navbar.php
@@ -0,0 +1,11 @@
+<li>
+	<a href="<?php echo URL; ?>">Home</a>	
+</li>
+
+<li>
+	<a href="<?php echo URL; ?>/pages/about">About</a>	
+</li>
+
+<li>
+	<a href="<?php echo URL; ?>/users/register">Register</a>
+</li>
