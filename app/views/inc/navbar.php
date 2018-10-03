86e2e837f7e4058026f687cf9e8915593078b3b8
sdufhboir <kevinab@stud.ntnu.no>
2018-10-03 11:22:21 +0200

    Added css for navbar and background

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,20 +1,63 @@
-<li>
-	<a href="<?php echo URL; ?>">Home</a>	
-</li>
+<style>
+	body {
+		background-color: grey;
+	}
 
-<li>
-	<a href="<?php echo URL; ?>/pages/about">About</a>	
-</li>
-<li>
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #333;
+	}
+
+	li {
+		float:  left;
+		display:inline;
+	}
+
+	li a {
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+	}
+
+	li a:hover {
+		background-color: #111;
+	}
+
+	table {
+		background-color: #333;
+	}
+
+	th {
+		color: white;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: white;
+	}
+</style>
+
+<ul>
+	<li>
+		<a href="<?php echo URL; ?>">Home</a>	
+	</li>
+
+	<li>
+		<a href="<?php echo URL; ?>/pages/about">About</a>	
+	</li>
+	<li>
 		<a href="<?php echo URL; ?>/threads/index">Threads</a>
-</li>
+	</li>
 <?php if(isset($_SESSION['user_id'])): ?>
-
 	
 	<li>
 		<a href="<?php echo URL; ?>/users/logout">Logout</a>
 	</li>
-
 
 <?php else : ?>
 	<li>
@@ -24,3 +67,4 @@
 		<a href="<?php echo URL; ?>/users/login">Login</a>
 	</li>
 <?php endif; ?>
+</ul>
