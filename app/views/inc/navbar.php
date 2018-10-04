4f585fe03c94f21f033b01cb3c5f4359f25db567
sdufhboir <kevinab@stud.ntnu.no>
2018-10-04 13:24:18 +0200

    css for login in navbar. View for add thread done

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -57,6 +57,107 @@
 	td {
 		color: white;
 	}
+	.login {
+		background: #333;
+		border: 1px solid #42464b;
+		border-radius: 6px;
+		height: 250px;
+		margin: 20px auto 0;
+		width: 298px;
+	}
+	.login h1 {
+		background-image: linear-gradient(top, #f1f3f3, #d4dae0);
+		border-bottom: 1px solid grey;
+		border-radius: 6px 6px 0 0;
+		box-sizing: border-box;
+		color: grey;
+		display: block;
+		height: 43px;
+		font: 600 14px/1 'Open Sans', sans-serif;
+		padding-top: 14px;
+		margin: 0;
+		text-align: center;
+		text-shadow: 0 -1px 0 rgba(0,0,0,0), 0 1px 0 #fff;
+	}
+
+	input[type="password"], input[type="text"] {
+		background: url('http://i.minus.com/ibhqW9Buanohx2.png') center left no-repeat, linear-gradient(top, #d6d7d7, #dee0e0);
+		border-radius: 4px;
+		box-shadow: 0 1px black;
+		box-sizing: border-box;
+		color: black;
+		height: 39px;
+		margin: 31px 0 0 29px;
+		padding-left: 37px;
+		transition: box-shadow 0.3s;
+		width: 240px;
+	}
+	input[type="password"]:focus, input[type="text"]:focus {
+		box-shadow: 0 0 4px 1px;
+		outline: 0;
+	}
+	.show-password {
+		display: block;
+		height: 16px;
+		margin: 26px 0 0 28px;
+		width: 87px;
+	}
+	input[type="checkbox"] {
+		cursor: pointer;
+		height: 16px;
+		opacity: 0;
+		position: relative;
+		width: 64px;
+	}
+	input[type="checkbox"]:checked {
+		left: 29px;
+		width: 58px;
+	}
+	.toggle {
+		background: url(http://i.minus.com/ibitS19pe8PVX6.png) no-repeat;
+		display: block;
+		height: 16px;
+		margin-top: -20px;
+		width: 87px;
+		z-index: -1;
+	}
+	input[type="checkbox"]:checked + .toggle { background-position: 0 -16px }
+	.forgot {
+		color: grey;
+		display: inline-block;
+		float: right;
+		font: 12px/1 sans-serif;
+		left: -19px;
+		position: relative;
+		text-decoration: none;
+		top: 5px;
+		transition: color .4s;
+	}
+	.forgot:hover { color: black }
+	input[type="submit"] {
+		width:240px;
+		height:35px;
+		display:block;
+		font-family:Arial, "Helvetica", sans-serif;
+		font-size:16px;
+		font-weight:bold;
+		color:black;
+		text-decoration:none;
+		text-transform:uppercase;
+		text-align:center;
+		text-shadow:1px 1px 0px grey;
+		padding-top:6px;
+		margin: 29px 0 0 29px;
+		position:relative;
+		cursor:pointer;
+		border: none;  
+		background-color: darkgrey;
+		border-top-left-radius: 5px;
+		border-top-right-radius: 5px;
+		border-bottom-right-radius: 5px;
+		border-bottom-left-radius:5px;
+		box-shadow: inset 0px 1px 0px grey, 0px 5px 0px 0px grey;
+	}
 </style>
 
 <ul>
