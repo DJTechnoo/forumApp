d243e30e8d3758a20df2e4e83c8b5bc7d22b8f43
sdufhboir <kevinab@stud.ntnu.no>
2018-10-04 10:45:02 +0200

    view threads done

--- app/views/threads/index.php
+++ app/views/threads/index.php
@@ -1,17 +1,17 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
 <h1><?php echo $data["title"];?></h1>
 <li><h2><a href="<?php echo URL; ?>/threads/addthread">Add new thread</a></h2></li><br/><br/><br/><br/><br/><br/>
-<?php foreach($data["threads"] as $thread) :?>
-
 <table>
 <tr>
-	<th scope='col'><h3>Threads</h3></th><th class='th' scope='col'>Posts</th>
+	<th scope='col'><font size='+4'>Threads</font></th><th class='th' scope='col'><font size='+4'>Posts</font></th>
 </tr>
-    <td><a href="<?php echo URL; ?>/posts/listposts/<?php echo $thread->threadid;?>"><?php echo "$thread->threadname" ?></a></td>
-	<td><p class='center'><?php echo $thread->postcount ?></p></td>    
-</table>
-
+<?php foreach($data["threads"] as $thread) :?>
+	<tr>
+		<td><a href="<?php echo URL; ?>/posts/listposts/<?php echo $thread->threadid;?>"><?php echo "<font size='+3'>$thread->threadname</font>" ?></a></td>
+		<td><p class='center'><?php echo $thread->postcount ?></p></td>
+	<tr/>
 <?php endforeach; ?>
+</table>
 <?php require APPROOT . "/views/inc/footer.php"; ?>
 <style>
 	.center {
