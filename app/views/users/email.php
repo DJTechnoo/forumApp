301701b5b882aee46eb164fb0f5164589a0c5b74
daniebk <daniel.b.kristensen@ntnu.no>
2018-10-04 11:11:59 +0200

    Check for email in registration

---
+++ app/views/users/email.php
@@ -0,0 +1,4 @@
+<?php require APPROOT . "/views/inc/header.php"; ?>
+<h1>Not a valid email address</h1>
+<p>The email <?php echo $data["email"]; ?> is not valid.</p>
+<?php require APPROOT . "/views/inc/footer.php"; ?>
