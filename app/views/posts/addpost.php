a88b94109cd604228a4d29b09408130326e12350
sdufhboir <kevinab@stud.ntnu.no>
2018-10-05 11:11:10 +0200

    View add post is done

--- app/views/posts/addpost.php
+++ app/views/posts/addpost.php
@@ -1,9 +1,19 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
 <h1>Add Post</h1>
-<form action="<?php echo URL;?>/posts/addpost/<?php echo $data["currentThread"];?>" method="post">
-	<label for="title">Post Title:</label>
-	<input type="text" name="posttitle">
-	<br>
-	<textarea cols="40" rows="5" name="posttext"></textarea>
+<form class="login" action="<?php echo URL;?>/posts/addpost/<?php echo $data["currentThread"];?>" method="post">
+	<input type="text" name="posttitle" placeholder="Post title">
+	<br/><br/>
+	<textarea class="margin" cols="31" rows="7" maxlength="255" name="posttext" placeholder="Post text"></textarea>
 	<input type="submit" value="Add">
 <?php require APPROOT . "/views/inc/footer.php"; ?>
+
+<style>
+	.login {
+		width: 300px;
+		height: 380px;
+	}
+	textarea {
+		margin-left: 2em;
+		height: 160px;
+	}
+</style>
