c3951fd5ad06cefb079dd02c2c87e34abeabbed1
sdufhboir <kevinab@stud.ntnu.no>
2018-10-05 11:19:41 +0200

    addpost cleanup

--- app/views/posts/addpost.php
+++ app/views/posts/addpost.php
@@ -2,8 +2,8 @@
 <h1>Add Post</h1>
 <form class="login" action="<?php echo URL;?>/posts/addpost/<?php echo $data["currentThread"];?>" method="post">
 	<input type="text" name="posttitle" placeholder="Post title">
-	<br/><br/>
-	<textarea class="margin" cols="31" rows="7" maxlength="255" name="posttext" placeholder="Post text"></textarea>
+	<br/>
+	<textarea cols="31" rows="7" maxlength="255" name="posttext" placeholder="Post text"></textarea>
 	<input type="submit" value="Add">
 <?php require APPROOT . "/views/inc/footer.php"; ?>
 
