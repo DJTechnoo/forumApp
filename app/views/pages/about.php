28eab69a4795f74abdea81f6b4117f432043b3d0
Loading323 <mustafaa@stud.ntnu.no>
2018-10-08 13:01:11 +0200

    neutering daniel

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -2,7 +2,7 @@
 <h1><?php echo $data["title"];?></h1>
 
 <p>This forum is made by:</p><br/>
-<p>Daniel Balle Kristensen</p>
+<p>Daniel B. Kristensen</p>
 <p>Kristian Alsgaard Skaue</p>
 <p>Mustafa Abdullah</p>
 <p>Askel Eirik Johansson</p>
